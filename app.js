7d0c1c658f7f0d41c20eaa396da31d87b36b9121
Beaky <retomlinson14@gmail.com>
2019-02-04 16:14:15 -0800

    Made buttons that redirect to other pages

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var pantry = require('./routes/pantry');
+var profile = require('./routes/profile');
 // Example route
 // var user = require('./routes/user');
 
@@ -35,6 +37,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
+app.get('/pantry', pantry.view);
+app.get('/profile', profile.view);
 // Example route
 // app.get('/users', user.list);
 
