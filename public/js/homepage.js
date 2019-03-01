d4a9af9f4dea8e4e415c6cfae5bb98797e02bf57
Rebecca <rtomlins@ucsd.edu>
2019-02-28 19:03:02 -0800

    Made filters a collapsable dropdown

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,26 +5,9 @@ $(document).ready(function() {
 function initializePage2() {
 	console.log("Javascriptsdfs connected!");
   $(".start-cooking").hide();
-  //save version for A/B testing
+  $(".recipe-link").click(handleAnalytics);
+}
 
-  /*
-  var version = $('.homepage-css').attr('id');
-  localStorage.setItem("version", version);
-
-  var newUrl = window.location.href;
-  //append query string to url
-  if(newUrl.includes("version=A") || newUrl.includes("version=B")) {
-
-  	//set to version B
-  	if(newUrl === "version=A" && version == "B"){
-  		window.location.href.replace("version=A", "version=B");
-  	} else if (newUrl === "version=B" && version == "A") {
-  		window.location.href.replace("version=B", "version=A");
-  	}
-  	
-  } else {
-  	window.location += "?version="+version;
-  }
-  console.log(newUrl)
-  */
+function handleAnalytics(){
+  ga("send", "event", 'recipe clicked', 'click');
 }
