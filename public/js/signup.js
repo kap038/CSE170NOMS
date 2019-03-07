f97a2e238ba8c984d943a03c56cd6788918cebf4
Kylee (Kai-ling) Peng <kap038@ucsd.edu>
2019-03-06 17:23:51 -0800

    tutorial structure set up

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,12 +4,51 @@ $(document).ready(function() {
 
 function initializePage() {
 	console.log("Javascript connected!");
+  //hide tutorial pages
+  $('#recipe-tutorial').hide();
+  $('#4u-tutorial').hide();
+  $('#fav-tutorial').hide();
+  $('#complete-tutorial').hide();
+
+  //click events
 	$('.select-type').click(handleRadio);
-  // $('#signup').click(handleSignup);
+  $('#signup').click(handleSignup);
+  $('#tutorial-1').click(handleTutorial1);
+  $('#tutorial-2').click(handleTutorial2);
+  $('#tutorial-3').click(handleTutorial3);
+  $('#tutorial-4').click(handleTutorial4);
 }
 
 function handleRadio(e){
 	var type = $(this).attr('id');
 	localStorage.setItem("diet", type)
+}
 
+function handleSignup(e){
+  e.preventDefault();
+  $('#recipe-tutorial').show();
+  $('#diet').hide();
+}
+
+function handleTutorial1(e){
+  e.preventDefault();
+  $('#recipe-tutorial').hide();
+  $('#4u-tutorial').show();
+}
+
+function handleTutorial2(e){
+  e.preventDefault();
+  $('#4u-tutorial').hide();
+  $('#fav-tutorial').show();
+}
+
+function handleTutorial3(e){
+  e.preventDefault();
+  $('#fav-tutorial').hide();
+  $('#complete-tutorial').show();
+}
+
+function handleTutorial3(e){
+  e.preventDefault();
+  window.location.href = "/homepage";
 }
